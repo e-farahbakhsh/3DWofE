57ded7f8c7102c71bb8dac4b03d60811226f1642
Ehsan Farahbakhsh <35557957+e-farahbakhsh@users.noreply.github.com>
2019-09-01 13:19:49 +0430

    Update 3DWofE_uncertainty.py

--- WofE/3DWofE_uncertainty.py
+++ WofE/3DWofE_uncertainty.py
@@ -6,26 +6,20 @@ import itertools
 import math
 
 # Threshold value for the target element
-threshold = 0.450155049344027
+threshold = 0.4 # Replace with your desired number
 # A list of threshold values for the evidential models with respect to the input csv file
-thresholds = [259.936009793948, 1424.6917112257, 73759.3058497416, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-              0.5, 0.5, 0.5, 0.5]
+thresholds = [] # Fill the list with your desired numbers
 # A list of the variances calculated for the positive weights of the evidential models with respect to the
 # input csv file
-var_w_pos = [0.000300289945997081, 0.00390523028643939, 0.000953113314073704, 0.0243526300306134,
-             0.309562145261069, 1.10843539887808, 0.266431393145319, 0.00752936671358174, 0.00354412099768136,
-             0.00528459369117716, 0.000910454827955843, 0.00013605728252983, 0.000105761473267968]
+var_w_pos = [] # Fill the list with your desired numbers
 # A list of the variances calculated for the negative weights of the evidential models with respect to the
 # input csv file
-var_w_neg = [0.000140916346670202, 0.0000982786652817642, 0.000107582462139783,
-             0.0000990506894969525, 0.0000970232620425332, 0.000105772386407877, 0.000131755982005783,
-             0.0000970679758929667, 0.0000985977549431225, 0.0000975996148877691, 0.000107107809844943,
-             0.000324342270126537, 0.00102296302959494]
+var_w_neg = [] # Fill the list with your desired numbers
 
 # Calculating the variance of each voxel in the evidential models
-input_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Input.csv")
+input_file = open("D:/Input.csv")
 input_reader = csv.reader(input_file)
-output_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Variance.csv", "wb")
+output_file = open("D:/Variance.csv", "wb")
 output_writer = csv.writer(output_file)
 row_temp = []
 for row in input_reader:
@@ -45,9 +39,9 @@ del output_file
 del output_writer
 
 # Calculating the total variance/uncertainty of each voxel
-input_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Variance.csv")
+input_file = open("D:/Variance.csv")
 input_reader = csv.reader(input_file)
-output_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Total Variance.csv", "wb")
+output_file = open("D:/Total Variance.csv", "wb")
 output_writer = csv.writer(output_file)
 row_temp = []
 for row in input_reader:
@@ -64,11 +58,11 @@ del output_file
 del output_writer
 
 # Calculating the studentized posterior probability
-input1_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Final/Posterior Probability.csv")
+input1_file = open("D:/Posterior Probability.csv")
 input1_reader = csv.reader(input1_file)
-input2_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Total Variance.csv")
+input2_file = open("D:/Total Variance.csv")
 input2_reader = csv.reader(input2_file)
-output_file = open("D:/PhD-AUT/Thesis/Phase 02_Nochun/WofE/Uncertainty/Studentized Posterior Probability.csv", "wb")
+output_file = open("D:/Studentized Posterior Probability.csv", "wb")
 output_writer = csv.writer(output_file)
 row_temp = []
 for row1, row2 in itertools.izip(input1_reader, input2_reader):
